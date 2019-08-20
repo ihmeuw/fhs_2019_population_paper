08327d538f66805192be6e18fa7604398236501f
fukutaki <fukutaki@uw.edu>
2019-08-20 11:31:31 -0700

    Added inline comments to abstract

--- plug/abstract.py
+++ plug/abstract.py
@@ -23,4 +23,39 @@ largest economy by XX but in the reference scenario the USA would once again
 become in the largest economy in XX.
 """
 
+# The global total fertility rate (TFR) in 2100 is forecasted to be 1.66 
+# (95% UI xx–xx).
 
+# In the reference forecast, the global population is projected
+# to peak in 2063 at 9.7 (UI)  billion people, and decline to 8.7 (UI) in 2100.
+
+# The reference projections fore the 5 five largest countries in 2100 are India
+# (1067 million ([UI), ], Nigeria (791 million[(UI])), China (716 million([UI]))
+# USA USA (335 million [(UI)]), and DR Congo (246 million [UI])
+
+# Findings also suggest a shifting age structure in many parts of the world,
+# with XX individuals above the age of 65,  and XX individuals below the age of
+# 20, globally in 2100.
+
+# By 2050, XX countries will have a TFR below replacement and XX below
+# replacement by 2100.
+
+# XX countries including China, XX, XX and XX in the reference scenario will
+# have declines of greater than 50% from 2017 by 2100.
+
+# Alternative scenarios suggest meeting the SDG targets for education and
+# contraceptive met need will result in a global population of 6.2 billion in
+# 2100, and 6.8 billion assuming 99th percentile rates of change in educational
+# attainment and met need for contraception.
+
+#  Forecasts from this study differ from the UNPD and the Wittgenstein Centre,
+# which project 10.9 billion and 9.3 billion people globally in 2100,
+# respectively
+
+# The difference with UNPD in 2100 is due to XX million fewer people in the
+# reference scenario in sub-Saharan Africa and XX million in other regions,
+# primarily due to the level of fertility achieved in below replacement
+# populations.
+
+# China is expected to become the largest economy by XX but in the reference
+# scenario the USA would once again become in the largest economy in XX.
