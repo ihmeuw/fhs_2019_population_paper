0f711c5425779f7a4f8d9885aa2cb646d51b0f5c
fukutaki <fukutaki@uw.edu>
2019-08-23 11:15:47 -0700

    Implemented migration functions for each sentence

--- plug/migration.py
+++ plug/migration.py
@@ -48,9 +48,94 @@ def _add_location_name(df):
     
     return df
 
+def _load_migration_rate():
+    pop_dir = FBDPath(
+        f"/{settings.BASELINE_VERSIONS['population_mean_ui'].gbd_round_id}/"
+        f"future/population/"
+        f"{settings.BASELINE_VERSIONS['population_mean_ui'].version}")
+    pop_path = pop_dir / "population_combined.nc"
+    pop_da = open_xr(pop_path).data.sel(
+        scenario=0, quantile="mean", location_id=COUNTRIES, sex_id=BOTH_SEX_ID,
+        age_group_id=ALL_AGE_ID)
+
+    mig_dir = FBDPath(
+        f"/{settings.BASELINE_VERSIONS['migration'].gbd_round_id}/"
+        f"future/migration/{settings.BASELINE_VERSIONS['migration'].version}")
+    mig_path = mig_dir / "migration.nc"
+    mig_da = open_xr(mig_path).data
+    mig_da = mig_da.mean('draw').sum(['sex_id', 'age_group_id'])
+    
+    mig_rate_da = mig_da / pop_da
+    return mig_rate_da
+
+# We forecasted 108 countries will have net migration rates between -1 and 1 per
+# 1000 population in 2100, and 164 countries between -2 and 2.
+def mig_rate_range(mig_rate_da):
+    mig_rate_da = mig_rate_da.sel(year_id=2100)
+    count_between_one = len(mig_rate_da.where(
+        (mig_rate_da<.001) & (mig_rate_da>-.001), drop=True).location_id)
+    count_between_two = len(mig_rate_da.where(
+        (mig_rate_da<.002) & (mig_rate_da>-.002), drop=True).location_id)
+    
+    print(f"We forecasted {count_between_one} countries will have net "
+          f"migration rates between -1 and 1 per 1000 population in 2100, and "
+          f"{count_between_two} countries between -2 and 2.")
+
+# The countries with the largest in-migration in absolute numbers in 2100 are
+# the United States, India, and China whereas out-migration will be largest in
+# Somalia, Philippines, and Bangladesh.
+def largest_counts():
+    mig_dir = FBDPath(
+        f"/{settings.BASELINE_VERSIONS['migration'].gbd_round_id}/"
+        f"future/migration/{settings.BASELINE_VERSIONS['migration'].version}")
+    mig_path = mig_dir / "migration.nc"
+    mig_da = open_xr(mig_path).data
+    mig_da = mig_da.mean('draw').sum(['sex_id', 'age_group_id'])
+    mig_da = mig_da.sel(year_id=2100)
+    mig_df = mig_da.to_dataframe()
+    mig_df = _add_location_name(mig_df)
+    mig_df = mig_df.sort_values(
+        by='value', ascending=False).reset_index()
+
+    first = mig_df.location_name[0]
+    second = mig_df.location_name[1]
+    third = mig_df.location_name[2]
+    
+    last = mig_df.location_name.iloc[-1]
+    second_last = mig_df.location_name.iloc[-2]
+    third_last = mig_df.location_name.iloc[-3]
+
+    print(f"The countries with the largest in-migration in absolute numbers in "
+          f"2100 are {first}, {second}, and {third} whereas out-migration will "
+          f"be largest in {last}, {second_last}, and {third_last}.")
+
+# Net in-migration rates are forecasted to be high in Timor-Leste, Israel, and
+# Canada, while out-migration rates are highest in Samoa, El Salvador, Jamaica,
+# and Syria.
+def largest_rates(mig_rate_da):
+    mig_rate_da = mig_rate_da.sel(year_id=2100)
+    mig_rate_df = mig_rate_da.to_dataframe()
+    mig_rate_df = _add_location_name(mig_rate_df)
+    mig_rate_df = mig_rate_df.sort_values(
+        by='value', ascending=False).reset_index()
+
+    first = mig_rate_df.location_name[0]
+    second = mig_rate_df.location_name[1]
+    third = mig_rate_df.location_name[2]
+
+    last = mig_rate_df.location_name.iloc[-1]
+    second_last = mig_rate_df.location_name.iloc[-2]
+    third_last = mig_rate_df.location_name.iloc[-3]
+    
+    print(f"Net in-migration rates are forecasted to be high in {first}, "
+          f"{second}, and {third}, while out-migration rates are highest in "
+          f"{last}, {second_last}, and {third_last}.")
 
 if __name__ == "__main__":
 
-	
+    mig_rate_da = _load_migration_rate()
+    mig_rate_range(mig_rate_da)
+    largest_counts()
+    largest_rates(mig_rate_da)
 
     great_job.congratulations()
