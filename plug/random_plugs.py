33978aa15f755d5b70691c3050b684904797ea50
fukutaki <fukutaki@uw.edu>
2019-08-28 09:56:36 -0700

    Pop decline pcts method is disgusting

--- plug/random_plugs.py
+++ plug/random_plugs.py
@@ -11,6 +11,14 @@ Limitations: But for countries such as Niger (190 [95% UI XX–XX] million in
 in 2100)  who will experience large population growth and while remaining
 low-income, there is a real possibility that such levels of population will not
 be sustainable
+
+Twenty five countries, including Japan, Thailand, Italy, Spain and Ukraine, are
+expected to decline in population by 50% or more. Another 33 countries will
+likely decline by 25 to 50%, including China with a 49% decline. Population
+percentage declines do not convey the associated profound shifts in age
+structure in these nations. Our findings suggest that the ratio of the
+population over 80 to the population under 15 will increase in these countries
+from XX today to XX (XX–XX) in 2100. 
 """
 import pandas as pd
 import xarray as xr
@@ -23,7 +31,7 @@ from fbd_core.etl.transformation import expand_dimensions
 from fbd_core.file_interface import FBDPath, open_xr, save_xr
 
 import settings
-
+import ipdb
 ALL_AGE_ID = 22
 BOTH_SEX_ID = 3
 
@@ -155,13 +163,115 @@ def pop_declines():
           f"declines in population but have not adopted immigration as a "
           f"strategy.\n")
 
+"""
+Twenty five countries, including Japan, Thailand, Italy, Spain and Ukraine, are
+expected to decline in population by 50% or more. Another 33 countries will
+likely decline by 25 to 50%, including China with a 49% decline. Population
+percentage declines do not convey the associated profound shifts in age
+structure in these nations. Our findings suggest that the ratio of the
+population over 80 to the population under 15 will increase in these countries
+from XX today to XX (XX–XX) in 2100.
+"""
+def pop_decline_pcts():
+    forecast_pop_dir = FBDPath(
+        f"/{settings.BASELINE_VERSIONS['population_mean_ui'].gbd_round_id}/"
+        f"future/population/"
+        f"{settings.BASELINE_VERSIONS['population_mean_ui'].version}")
+    forecast_pop_path = forecast_pop_dir / "population_combined.nc"
+    forecast_pop_da = open_xr(forecast_pop_path).data.sel(
+        sex_id=BOTH_SEX_ID, scenario=0, quantile="mean", location_id=COUNTRIES)
+
+    past_pop_dir = FBDPath(
+        f"/{settings.PAST_VERSIONS['population'].gbd_round_id}/"
+        f"past/population/"
+        f"{settings.PAST_VERSIONS['population'].version}")
+    past_pop_path = past_pop_dir / "population.nc"
+    past_pop_da = open_xr(past_pop_path).data.sel(
+        sex_id=BOTH_SEX_ID, location_id=COUNTRIES)
+
+    pct_decline_da = 1-(forecast_pop_da.sel(age_group_id=ALL_AGE_ID, year_id=2100)/past_pop_da.sel(
+        age_group_id=ALL_AGE_ID, year_id=2017))
+    decline_over_50_val = (pct_decline_da >= .5).sum().values.item(0)
+    decline_over_50_locs = list(pct_decline_da.where(
+        pct_decline_da >= .5, drop=True).location_id.values)
+    decline_25_to_50_val = ((pct_decline_da > .25) & (pct_decline_da < .5)).sum(
+        ).values.item(0)
+    decline_25_to_50_locs = list(pct_decline_da.where(
+        (pct_decline_da > .25) & (pct_decline_da < .5), drop=True).location_id.values)
+    decline_over_25_locs = decline_over_50_locs + decline_25_to_50_locs
+
+    forecast_pop_over_50 = forecast_pop_da.sel(
+        location_id=decline_over_50_locs, year_id=2100)
+    past_pop_over_50 = past_pop_da.sel(
+        location_id=decline_over_50_locs, year_id=2017)
+    forecast_pop_25_to_50 = forecast_pop_da.sel(
+        location_id=decline_25_to_50_locs, year_id=2100)
+    past_pop_25_to_50 = past_pop_da.sel(
+        location_id=decline_25_to_50_locs, year_id=2017)
+    forecast_pop_over_25 = forecast_pop_da.sel(
+        location_id=decline_over_25_locs, year_id=2100)
+    past_pop_over_25 = past_pop_da.sel(
+        location_id=decline_over_25_locs, year_id=2017)
+
+    over_80_age_ids = [30, 31, 32, 235]
+    under_15_age_ids = [2, 3, 4, 5, 6, 7]
+    
+    ratio_2100 = (forecast_pop_over_25.sel(age_group_id=over_80_age_ids).sum('age_group_id').sum('location_id') /
+        forecast_pop_over_25.sel(age_group_id=under_15_age_ids).sum('age_group_id').sum('location_id')).values.item(0)
+    ratio_2017 = (past_pop_over_25.sel(age_group_id=over_80_age_ids).sum('age_group_id').sum('location_id') /
+        past_pop_over_25.sel(age_group_id=under_15_age_ids).sum('age_group_id').sum('location_id')).values.item(0)
+
+    forecast_pop_upper = open_xr(forecast_pop_path).data.sel(
+        sex_id=BOTH_SEX_ID, scenario=0, quantile="upper",
+        location_id=decline_over_50_locs)
+    forecast_pop_lower = open_xr(forecast_pop_path).data.sel(
+        sex_id=BOTH_SEX_ID, scenario=0, quantile="lower",
+        location_id=decline_over_50_locs)
+
+    ratio_2100_lower = (forecast_pop_lower.sel(age_group_id=over_80_age_ids).sum('age_group_id').sum('location_id') /
+        forecast_pop_lower.sel(age_group_id=under_15_age_ids).sum('age_group_id').sum('location_id')).values.item(0)
+    ratio_2100_upper = (forecast_pop_upper.sel(age_group_id=over_80_age_ids).sum('age_group_id').sum('location_id') /
+        forecast_pop_upper.sel(age_group_id=under_15_age_ids).sum('age_group_id').sum('location_id')).values.item(0)
+
+    forecast_pop_draw_dir = FBDPath(
+        f"/{settings.BASELINE_VERSIONS['population'].gbd_round_id}/"
+        f"future/population/"
+        f"{settings.BASELINE_VERSIONS['population'].version}")
+    forecast_pop_draw_path = forecast_pop_draw_dir / "population.nc"
+    forecast_pop_draw = open_xr(forecast_pop_draw_path).data.sel(scenario=0,
+        location_id=decline_over_25_locs, year_id=2100).sum('sex_id')
+    
+    ratio_draws = (forecast_pop_draw.sel(age_group_id=over_80_age_ids).sum(
+        'age_group_id').sum('location_id') /
+        forecast_pop_draw.sel(age_group_id=under_15_age_ids).sum(
+            'age_group_id').sum('location_id'))
+    
+    ratio_2100_lower = ratio_draws.quantile(q=.025).values.item(0)
+    ratio_2100_upper = ratio_draws.quantile(q=.975).values.item(0)
+
+    ratio_2017 = _helper_round(ratio_2017, 1)
+    ratio_2100 = _helper_round(ratio_2100, 1)
+    ratio_2100_lower = _helper_round(ratio_2100_lower, 1)
+    ratio_2100_upper = _helper_round(ratio_2100_upper, 1)
+
+
+    print(f"{decline_over_50_val} countries, including Japan, Thailand, Italy, "
+          f"Spain and Ukraine, are expected to decline in population by 50% or "
+          f"more. Another {decline_25_to_50_val} countries will likely decline "
+          f"by 25 to 50%, including China with a 49% decline. Population "
+          f"percentage declines do not convey the associated profound shifts "
+          f"in age structure in these nations. Our findings suggest that the "
+          f"ratio of the population over 80 to the population under 15 will "
+          f"increase in these countries from {ratio_2017} today to "
+          f"{ratio_2100} ({ratio_2100_lower}–{ratio_2100_upper}) in 2100.")
+
 
 # Japan and Russia pop rank 2017 vs 2100
 def japan_russia_pop():
     # This one isn't direct output needs user to read index
     past = xr.open_dataarray(
         f"/ihme/forecasting/data/5/past/population/"
-        f"{settings.PAST_VERSIONS["population"].version}/population.nc")
+        f"{settings.PAST_VERSIONS['population'].version}/population.nc")
     past = past.sel(age_group_id=22, sex_id=3, location_id=COUNTRIES,
         year_id=2017)
     pastdf = past.to_dataframe()
@@ -172,7 +282,7 @@ def japan_russia_pop():
 
     forecast = xr.open_dataarray(
         f"/ihme/forecasting/data/5/future/population/"
-        f"{settings.BASELINE_VERSIONS["population_mean_ui"].version}/"
+        f"{settings.BASELINE_VERSIONS['population_mean_ui'].version}/"
         f"population.nc")
     forecast = forecast.sel(age_group_id=22, sex_id=3, location_id=COUNTRIES,
         year_id=2100)
@@ -183,10 +293,13 @@ def japan_russia_pop():
     forecastdf[forecastdf['location_name']=="Japan"]
     forecastdf[forecastdf['location_name']=="Russia"]
 
+
+
 if __name__ == "__main__":
 
-    sing_tai_tfr()
-    pops_2100()
-    pop_declines()
+    # sing_tai_tfr()
+    # pops_2100()
+    # pop_declines()
+    pop_decline_pcts()
 
     great_job.congratulations()
